57f19a787f6eebae99800a589611b7f577bdb2a5
Devin Watson <devinwatson015@gmail.com>
2016-07-13 09:25:44 +0300

    intro page is made responsive, previous-work page is modified.

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -25,8 +25,6 @@ $(function(){
 
   "use strict";
 
-    var topoffset = 50;      
-
     var height = $(window).height();
     $(".full-height").css("height", height);
 
@@ -50,61 +48,7 @@ $(function(){
     	$(".sidebar .social").css("display", "table-cell");
     }
     });
-//h1 disappear
-    var wHeight = $(window).height();
-
-    if(wHeight < 500) {
-    $(".content-wrapper h1").text(" ");
-    }
-    $(window).resize(function(){
-      var wHeight = $(window).height();
-      if(wHeight < 500) {
-    $(".content-wrapper h1").text(" ");
-
-    } else{
-      $(".content-wrapper h1").text("devin watson");
-
-    }
-    });
-
-
-// //intro image resize
-
-//     var window_height = $(window).height();
-//     var window_width = $(window).width();
-//     var divided = window_height / window_width;
-//     var skew_value = divided.val();
-
-//     $(".intro-image").css({
-//       "transform": "skewX(-" + skew_value + ")",
-//       "-webkit-transform" : "skewX(-" + skew_value + ")", 
-//       "-ms-transform":"skewX(-" + skew_value + ")"
-//     });
-//       $(".intro-image img").css({
-//       "transform": "skewX(" + skew_value + ")",
-//       "-webkit-transform" : "skewX(" + skew_value + ")", 
-//       "-ms-transform":"skewX(" + skew_value + ")"
-//     });
-
-//     $(window).resize(function(){
-//       var window_height = $(window).height();
-//       var window_width = $(window).width();
-//       var divided = window_height / window_width;
-//       var skew_value = divided.val();
-
-//       $(".intro-image").css({
-//       "transform": "skewX(-" + skew_value + ")",
-//       "-webkit-transform" : "skewX(-" + skew_value + ")", 
-//       "-ms-transform":"skewX(-" + skew_value + ")"
-//     });
-
-//       $(".intro-image img").css({
-//       "transform": "skewX(" + skew_value + ")",
-//       "-webkit-transform" : "skewX(" + skew_value + ")", 
-//       "-ms-transform":"skewX(" + skew_value + ")"
-//     });
-
-
+  
 });//document load
 
 }).call(this,typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
