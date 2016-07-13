db2bac6809dc4a2eba08d5f6a9d27a6876da964a
Devin Watson <devinwatson015@gmail.com>
2016-07-13 19:19:44 +0300

    few responsive design issues are solved for <400px screens and files are compressed for trial

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -48,27 +48,7 @@ $(function(){
     	$(".sidebar .social").css("display", "table-cell");
     }
     });
-  
-    var $quote = $(".skills-dots"),
-    mySplitText = new SplitText($quote, {type:"words"}),
-    splitTextTimeline = new TimelineLite();
-
-TweenLite.set($quote, {perspective:400});
-
-//kill any animations and set text back to its pre-split state
-function kill(){
-  splitTextTimeline.clear().time(0);
-  mySplitText.revert();
-}
-$("#charsWordsLines").click(function() {
-  kill();
-  mySplitText.split({type:"chars, words, lines"}) 
-  splitTextTimeline.staggerFrom(mySplitText.chars, 0.2, {autoAlpha:0, scale:4, force3D:true}, 0.01, 0.5)
-    .staggerTo(mySplitText.words, 0.1, {color:"#8FE402", scale:0.9}, 0.1, "words")
-    .staggerTo(mySplitText.words, 0.2, {color:"white", scale:1}, 0.1, "words+=0.1")
-    .staggerTo(mySplitText.lines, 0.5, {x:100, autoAlpha:0}, 0.2) 
-});
-});//document load
+  });//document load
 
 }).call(this,typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"jquery":2}],2:[function(require,module,exports){
