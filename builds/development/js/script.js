cc7493b36fb73f4e4a56524b3cbc26755a78065c
Devin Watson <devinwatson015@gmail.com>
2016-08-28 18:30:18 +0300

    sidebar social icons are removed, url chane on scroll is added

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -24,70 +24,99 @@ $(function(){
     var $wHeight = $(window).height();
     var $toggleButton = $(".toggle");
     var $toggleX = $(".toggleX");
-    var $sideSocial = $("#sidebar-social li img");
     var $social = $("#social-nav li img");
     var $landscape = $(".landscape");
     var $sidebar = $(".sidebar");
 
-    if($wHeight < 600) {
+    if($wHeight > 600) {
+      $toggleButton.css("display", "block");
+      $toggleX.css("display", "block");
+      $social.css("visibility", "visible");
+      $landscape.css("display", "none");
+      $sidebar.css("display", "block");
+    } else {
       $toggleButton.css("display", "none");
       $toggleX.css("display", "none");
       $landscape.css("display", "block");
       $social.css("visibility", "hidden");
       $sidebar.css("display", "none");
-      $sideSocial.css("visibility", "hidden");
-    } else {
-      $toggleButton.css("display", "block");
-      $toggleX.css("display", "block");
-      $social.css("visibility", "visible");
-      $sideSocial.css("visibility", "visible");
-      $landscape.css("display", "none");
-      $sidebar.css("display", "block");
     }//else
 
     $(window).resize(function(){
       var $wHeight = $(window).height();
       var $toggleButton = $(".toggle");
       var $toggleX = $(".toggleX");
-      var $sideSocial = $("#sidebar-social li img");
       var $social = $("#social-nav li img");
       var $landscape = $(".landscape");
       var $sidebar = $(".sidebar");
 
-      if($wHeight < 600) {
+      if($wHeight > 600) {
+        $toggleButton.css("display", "block");
+        $toggleX.css("display", "block");
+        $social.css("visibility", "visible");
+        $landscape.css("display", "none");
+        $sidebar.css("display", "block");
+      } else {
         $toggleButton.css("display", "none");
         $toggleX.css("display", "none");
         $landscape.css("display", "block");
         $social.css("visibility", "hidden");
         $sidebar.css("display", "none");
-        $sideSocial.css("visibility", "hidden");
-      } else {
-        $toggleButton.css("display", "block");
-        $toggleX.css("display", "block");
-        $social.css("visibility", "visible");
-        $sideSocial.css("visibility", "visible");
-        $landscape.css("display", "none");
-        $sidebar.css("display", "block");
       }//else
     }); //landscape navigation
-    
-//smooth scroll
-    $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-   
+
   });//document load
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+$(document).bind('scroll',function(e){
+    $('section').each(function(){
+        if (
+           $(this).offset().top < window.pageYOffset + 10
+//begins before top
+        && $(this).offset().top + $(this).height() > window.pageYOffset + 10
+//but ends in visible area
+//+ 10 allows you to change hash before it hits the top border
+        ) {
+            window.location.hash = $(this).attr('id');
+        }
+    });
+});
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.sidebar a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.sidebar ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
 
 },{"jquery":2}],2:[function(require,module,exports){
 /*!
